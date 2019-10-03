e72d432998a55a125ed0232581ee0f4cfa178449
Daniel <daniel0000@ukr.net>
2019-10-03 21:39:30 +0300

    +

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const express = require('express')
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const todoRouter = require('./routes/todos.js')
 
-const PORT = 3000
+const PORT = 3000;
 
-const app = express()
+const app = express();
 
-app.listen(PORT, ()=> {
-    console.log('ok')
-})
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(todoRouter);
+
+async function start() {
+    try {
+        await mongoose.connect('mongodb+srv://daniel000:1FYFVf12@cluster0-w4m3i.mongodb.net/admin?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+        app.listen(PORT, ()=> {
+            console.log('ok')
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start()
