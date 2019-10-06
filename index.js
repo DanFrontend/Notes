4cc99d3461d9dec2d462ef2f59ca08cdc7862317
bandura <banduraweb@ukr.net>
2019-10-06 22:21:03 +0300

    :-)

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const todoRouter = require('./routes/todos.js')
-
-const PORT = 3000;
-
+const todoRouter = require('./routes/todos.js');
 const app = express();
-
+const port = 3010;
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "pug");
 app.use(todoRouter);
 
-async function start() {
-    try {
-        await mongoose.connect('mongodb+srv://daniel000:1FYFVf12@cluster0-w4m3i.mongodb.net/admin?retryWrites=true&w=majority', {
-            useNewUrlParser: true,
-            useFindAndModify: false
-        })
-        app.listen(PORT, ()=> {
-            console.log('ok')
-        })
-    } catch (e) {
-        console.log(e);
+(async ()=> {
+    {
+        try {
+            await mongoose.connect('mongodb+srv://daniel000:1FYFVf12@cluster0-w4m3i.mongodb.net/admin?retryWrites=true&w=majority', {
+                useNewUrlParser: true,
+                useFindAndModify: false
+            });
+            app.listen(port, () => console.log('Server has been started...'));
+        } catch (e) {
+            console.log(e)
+        }
     }
-}
+})();
 
-start()
+
+
+
--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,20 @@
 const { Router } = require('express');
+// const L = require('../models/list');
+// const N = require('../models/note');
 const router = Router();
 
-router.get('/', (req, res)=> {
-    res.render('index')
-})
+router.get('/',  (req, res)=> {
 
-module.exports = router
+    res.render('index')
+});
+
+router.get('/create-list', (req, res)=> {
+    res.render('create-list')
+});
+
+router.get('/create-notes', (req, res)=> {
+    res.render('create-notes')
+});
+
+module.exports = router;
+
